73d252dada6c8eb9d036ea54f928525aa91ea5ab
Blaine Price <1wbprice@gmail.com>
2017-09-14 15:49:43 -0400

    Debug remote creation

--- app/controllers/remote/index.js
+++ app/controllers/remote/index.js
@@ -1,9 +1,26 @@
 'use strict';
 
-function fetchRemotes() {
+const Wreck = require('wreck');
+const Boom = require('boom');
 
+const API_BASE = process.env.API_BASE;
+
+function createRemote(request, reply) {
+    const { owner } = request.params;
+    const { temperature, location } = request.params.payload;
+
+    Wreck.post(`${API_BASE}/remotes`, {
+        owner,
+        temperature,
+        location
+    }, (err, res, payload) => {
+        if (err) {
+            return reply(Boom.badRequest(error.message));
+        }
+        reply(payload);
+    });
 }
 
 module.exports = {
-    fetchRemotes
+    createRemote
 }
