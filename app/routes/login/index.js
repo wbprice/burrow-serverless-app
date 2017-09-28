1e666bb74b47b17f62bcd2a67589b2d12b1dd8a3
Blaine Price <1wbprice@gmail.com>
2017-09-28 13:08:45 -0400

    Clear some errors

--- app/routes/login/index.js
+++ app/routes/login/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const path = require('path');
-
 const {
     API_ROOT,
     FB_APP_ID,
@@ -12,9 +10,7 @@ function handler(request, reply) {
     const user = request.yar.get('user');
 
     return reply.view('login', {
-        user,
-        FB_APP_ID,
-        SOCIAL_LOGIN_REDIRECT
+        user
     });
 }
 
--- app/routes/signup/index.js
+++ app/routes/signup/index.js
@@ -1,17 +1,9 @@
 'use strict';
 
-const {
-    API_ROOT,
-    FB_APP_ID,
-    SOCIAL_LOGIN_REDIRECT,
-} = process.env;
-
 function handler(request, reply) {
     const user = request.yar.get('user');
     return reply.view('signup', { 
-        user,
-        FB_APP_ID,
-        SOCIAL_LOGIN_REDIRECT: path.join(API_ROOT, SOCIAL_LOGIN_REDIRECT)
+        user
     });
 }
 
