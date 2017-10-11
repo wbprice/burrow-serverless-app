6a06c1a2a256a427fbb0ca77769f1394000d4a9e
Blaine Price <1wbprice@gmail.com>
2017-10-11 14:53:43 -0400

    potato.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const path = require('path');
 const compileTemplates = require('./tasks/compileTemplates');
 
 gulp.task('compileTemplates', () => compileTemplates({
-    src: path.join(__dirname, 'app/assets/js/pages/*.js'),
+    src: path.join(__dirname, 'app/assets/js/pages/Index.js'),
     dest: './dist'
 }));
 
--- tasks/compileTemplates.js
+++ tasks/compileTemplates.js
@@ -2,11 +2,33 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const webpack = require('gulp-webpack');
+const through2 = require('through2');
 
 function compileTemplates(opts) {
     return gulp.src(opts.src)
-        .pipe(babel({
-            presets: ['env', 'react']
+        .pipe(webpack({
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['env']
+                            }
+                        }
+                    }
+                ]
+            } 
+        }))
+        .pipe(through2.obj(function(chunk, env, callback) {
+            const module = eval(chunk.contents.toString());
+            console.log(module);
+
+            this.push(chunk);
+            callback();
         }))
         .pipe(gulp.dest(opts.dest))
 }
