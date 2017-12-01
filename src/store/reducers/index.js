f084643c8b489ef68dce2fd31723c9527e1b8801
Blaine Price <1wbprice@gmail.com>
2017-11-30 20:45:41 -0500

    Add user actions and reducer.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -4,10 +4,12 @@ import { routerReducer } from 'react-router-redux';
 import login from './login-reducer';
 import signup from './signup-reducer';
 import tokens from './token-reducer'
+import user from './user-reducer';
 
 export default combineReducers({
     login,
     signup,
     tokens,
+    user,
     router: routerReducer
 })
