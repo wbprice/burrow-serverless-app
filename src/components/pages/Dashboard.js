5b38e877acb3089e6bec155e6404d88dd76a78a1
Blaine Price <1wbprice@gmail.com>
2017-12-04 15:50:19 -0500

    Write actions for creating and fetching remotes.

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -4,17 +4,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import RemoteForm from './../organisms/RemoteForm';
+import { fetchRemotes } from './../../store/actions/dashboard-actions'
 
 class Dashboard extends Component {
+
+    componentDidMount() {
+        this.props.dispatch(fetchRemotes(this.props.idToken))
+    }
+
     render() {
         return (
-            <div class="container">
+            <div className="container">
                 <h1>Dashboard</h1>
                 <p>Manage your remotes here</p>
 
                 {this.props.remotes.map(remote => {
                     return (
                         <RemoteForm
+                            key={remote.id}
+                            dispatch={this.props.dispatch}
                             id={remote.id}
                             temperature={remote.temperature}
                             name={remote.name}
@@ -26,7 +34,12 @@ class Dashboard extends Component {
                     <div className="card">
                         <h2>DEBUG</h2>
                         <p>There aren't any remotes!  You should create one.</p>
-                        <RemoteForm />
+                        <RemoteForm 
+                            idToken={this.props.idToken}
+                            dispatch={this.props.dispatch}
+                            temperature={this.props.debug.temperature}
+                            name={this.props.debug.name}
+                        />
                     </div>
                 }
             </div>
@@ -40,6 +53,8 @@ Dashboard.propTypes = {
 
 export default connect((state) => {
     return {
-        remotes: state.dashboard.remotes
+        remotes: state.dashboard.remotes,
+        debug: state.dashboard.debug,
+        idToken: state.user.tokens.idToken.jwtToken
     }
 })(Dashboard)
--- src/store/reducers/dashboard-reducer.js
+++ src/store/reducers/dashboard-reducer.js
@@ -1,9 +1,69 @@
+import update from 'immutability-helper'
+
+import {
+    SET_REMOTE_NAME,
+    SET_REMOTE_TEMPERATURE,
+    FETCH_REMOTES_REQUEST,
+    FETCH_REMOTES_SUCCESS,
+    FETCH_REMOTES_FAILURE 
+} from './../actions/dashboard-actions';
+
 const initialState = {
-    remotes: []
+    remotes: [],
+    debug: {
+        temperature: {
+            value: '',
+            error: ''
+        },
+        name: {
+            value: '',
+            error: ''
+        }
+    }
+}
+
+function mungeRemote(remote) {
+    return Object.assign({}, remote, {
+        name: {
+            value: remote.name,
+            error: ''
+        },
+        temperature: {
+            value: remote.temperature
+        }
+    })
 }
 
 export default function dashboardReducer(state = initialState, action) {
     switch(action.type) {
+
+        case FETCH_REMOTES_REQUEST:
+            return state;
+
+        case FETCH_REMOTES_SUCCESS:
+            return update(state, {
+                remotes: { $set: action.response.map(mungeRemote) }
+            })
+
+        case FETCH_REMOTES_FAILURE:
+            return state;
+
+        case SET_REMOTE_NAME:
+            if (!action.id) {
+                return update(state, {
+                    debug: { name: {$set: {value: action.name}}}
+                })
+            }
+            return state
+
+        case SET_REMOTE_TEMPERATURE:
+            if (!action.id) {
+                return update(state, {
+                    debug: { temperature: {$set: {value: action.temperature}}}
+                })
+            }
+            return state
+
         default:
             return state;
     }
